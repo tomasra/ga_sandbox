060c65ea09f88066c73bc6ff5e8aff10888ad17a
Tomas Raila <tomas.raa@gmail.com>
2014-05-05 23:40:40 +0300

    nasty bug of never happening crossover

--- lib/crossovers/one_point.py
+++ lib/crossovers/one_point.py
@@ -8,7 +8,7 @@ class OnePointCrossover(Crossover):
     def _run_specific(self, parent1, parent2):
         cross_index = random.randint(0, len(parent1) - 1)
         parent1_part1, parent1_part2 = parent1.split(cross_index)
-        parent2_part1, parent2_part2 = parent1.split(cross_index)
+        parent2_part1, parent2_part2 = parent2.split(cross_index)
         offspring1 = parent1_part1.concat(parent2_part2)
         offspring2 = parent2_part1.concat(parent1_part2)
         return offspring1, offspring2
