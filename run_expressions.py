1efae545565108972c920f51deb322df072c93c5
Tomas Raila <tomas.raa@gmail.com>
2014-05-19 21:06:50 +0300

    something done with arithmetic expression solutions, not sure if its working

--- run_expressions.py
+++ run_expressions.py
@@ -1,4 +1,4 @@
-#!/venv/bin/python
+#!venv/bin/python
 
 from lib.algorithm import Algorithm
 from lib.crossovers.one_point import OnePointCrossover
@@ -6,15 +6,19 @@ from lib.selections.roulette_wheel import RouletteWheelSelection
 from lib.solutions.arithmetic_expression import ArithExpSolutionFactory
 
 alg = Algorithm(
-    ArithExpSolutionFactory(target=100, length=17),
+    ArithExpSolutionFactory(target=100, length=13),
     OnePointCrossover(rate=0.7),
     RouletteWheelSelection(),
     population_size=50,
     mutation_rate=0.005
 )
 
-for i in xrange(20):
+for i in xrange(15):
     alg.run()
-    s = "Average fitness: " + str(alg.population.average_fitness)
-    s += ", best solution: " + str(alg.population.best_solution)
+    s = "Average fitness: " + "{:1.3f}".format(
+        alg.population.average_fitness)
+    s += ", Top fitness: " + "{:1.3f}".format(
+        alg.population.best_solution.fitness)
+    s += ", best solution: " + str(
+        alg.population.best_solution)
     print s
