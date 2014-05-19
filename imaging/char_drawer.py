7a23f7cea1d546ace4e3406d5fb8aca8fa27ee18
Tomas Raila <tomas.raa@gmail.com>
2014-05-19 22:40:11 +0300

    making a nice mosaic (even with borders) from multiple char images

--- imaging/char_drawer.py
+++ imaging/char_drawer.py
@@ -72,3 +72,65 @@ class CharDrawer(object):
             np_data[:, :, i]
             for i in xrange(np_data.shape[2])
         ]
+
+    @staticmethod
+    def create_mosaic(images, width, height, borders=True):
+        """
+        Makes a width x height mosaic
+        from provided images (lists of numpy arrays)
+        """
+        shapes = set([
+            plane.shape
+            for image in images
+            for plane in image
+        ])
+        planes = set([len(image) for image in images])
+        if len(planes) > 1:
+            raise ValueError(
+                "Images should have the same number of color planes")
+        if len(shapes) > 1:
+            raise ValueError(
+                "Images should of the same dimensions")
+        elif len(images) > (width * height):
+            raise ValueError(
+                "Image count is larger than mosaic dimensions")
+        else:
+            shape, planes = list(shapes)[0], list(planes)[0]
+            image_width, image_height = shape[1], shape[0]
+            mosaic_width = image_width * width
+            mosaic_height = image_height * height
+            mosaic = [
+                np.zeros((mosaic_height, mosaic_width), dtype=np.uint8)
+                for i in xrange(planes)
+            ]
+            for idx, image in enumerate(images):
+                # Image indexes (0-based) in the mosaic
+                mosaic_x, mosaic_y = idx % width, idx / width
+                top_left_x = mosaic_x * image_width
+                top_left_y = mosaic_y * image_height
+                # Copy all planes
+                for plane_idx in xrange(planes):
+                    mosaic[plane_idx][
+                        top_left_y:top_left_y + image_height,
+                        top_left_x:top_left_x + image_width
+                    ] = image[plane_idx]
+                if borders:
+                    # Right and bottom border for each image
+                    for plane_idx in xrange(planes):
+                        # Right
+                        mosaic[plane_idx][
+                            top_left_y:top_left_y + image_height,
+                            # "Index out of bounds" happens without this one
+                            top_left_x + image_width - 1
+                        ] = 0
+                        # Bottom
+                        mosaic[plane_idx][
+                            top_left_y + image_height - 1,
+                            top_left_x:top_left_x + image_width
+                        ] = 0
+            # Top and left border for whole mosaic
+            if borders:
+                for plane_idx in xrange(planes):
+                    mosaic[plane_idx][0, 0:mosaic_width] = 0
+                    mosaic[plane_idx][0:mosaic_height, 0] = 0
+            return mosaic
--- run_chars.py
+++ run_chars.py
@@ -4,11 +4,27 @@ from imaging.char_drawer import CharDrawer
 import imaging.noises as noises
 from imaging.utils import render_image
 
-chars = CharDrawer()
-bw, color = chars.create_pair('K')
+char_drawer = CharDrawer()
+bw, color = char_drawer.create_pair('F')
 # noisified = noises.salt_and_pepper(color, 0.1)
 noisified = noises.gaussian(color, sigma=15.0)
-render_image(bw)
-render_image(color)
-render_image(noisified)
+# render_image(bw)
+# render_image(color)
+# render_image(noisified)
 # import pdb; pdb.set_trace()
+
+chars = "ABCDEF"
+pairs = [
+    char_drawer.create_pair(char)
+    for char in chars
+]
+bws = [pair[0] for pair in pairs]
+colors = [pair[1] for pair in pairs]
+
+bw_mosaic = CharDrawer.create_mosaic(bws, 3, 2)
+color_mosaic = CharDrawer.create_mosaic(colors, 2, 3)
+
+# import pdb; pdb.set_trace()
+
+render_image(bw_mosaic)
+render_image(color_mosaic)
