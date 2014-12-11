46d630e8e42c9add9fb22838490755f5f8d14c0e
Tomas Raila <tomas.raa@gmail.com>
2014-12-11 08:45:27 +0200

    Generate noise with scikit-image

--- projects/denoising/imaging/noises.py
+++ projects/denoising/imaging/noises.py
@@ -1,37 +1,18 @@
-import numpy as np
+from skimage.util import random_noise, img_as_ubyte
 from projects.denoising.imaging.image import Image
 
 # Override if needed
-_randomizer = np.random.RandomState()
+_rng_seed = None
 
 
-def salt_and_pepper(image, probability):
-    new_image = Image(image.copy())
-    for idx_channel, channel in enumerate(new_image.channels):
-        for idx_row, row in enumerate(channel):
-            for idx_col, col in enumerate(row):
-                # if rnd.random() < probability:
-                if _randomizer.random_sample() < probability:
-                    # Make the noise here
-                    # either white or black pixel
-                    # bit = rnd.randint(0, 1)
-                    bit = _randomizer.random_integers(0, 1)
-                    new_image[idx_row, idx_col, idx_channel] = 255 * bit
-    return new_image
+def salt_and_pepper(image, amount=0.05):
+    noisified = random_noise(
+        image, mode='s&p', seed=_rng_seed, amount=amount)
+    return Image(img_as_ubyte(noisified))
 
 
-def gaussian(image, mu=0.0, sigma=10.0):
-    new_image = Image(image.copy())
-    for idx_channel, channel in enumerate(new_image.channels):
-        for idx_row, row in enumerate(channel):
-            for idx_col, point in enumerate(row):
-                # new_point = point + rnd.gauss(mu, sigma)
-                new_point = point + _randomizer.normal(mu, sigma)
-                # Pixel value should be in [0; 255] interval
-                if new_point > 255:
-                    new_point = 255
-                elif point < 0:
-                    new_point = 0
-                new_image[idx_row, idx_col, idx_channel] = new_point
-
-    return new_image
+def gaussian(image, mean=0.0, var=0.01):
+    noisified = random_noise(
+        image, mode='gaussian', seed=_rng_seed,
+        mean=mean, var=var)
+    return Image(img_as_ubyte(noisified))
