9d297144a661f3f711aec1518a13c6e4bbaeca69
Tomas Raila <tomas.raa@gmail.com>
2014-12-15 10:52:07 +0200

    Deleted some old experiment scripts

--- projects/denoising/experiments/parse_all_results.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python parse_results.py results/snp_with_targets.pickle
-python parse_results.py results/gaussian_with_targets.pickle
-python parse_results.py results/snp_without_targets.pickle
-python parse_results.py results/gaussian_without_targets.pickle
