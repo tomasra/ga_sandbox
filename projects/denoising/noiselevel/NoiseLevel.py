26d65893ee6a5aeb8ceb1e9498fded777c993e8c
Tomas Raila <tomas.raa@gmail.com>
2015-03-03 17:01:10 +0200

    Noise estimation code fixes - now working more-or-less correctly

--- projects/denoising/noiselevel/NoiseLevel.py
+++ projects/denoising/noiselevel/NoiseLevel.py
@@ -1,6 +1,9 @@
 
 import numpy as np
 import scipy
+import scipy.ndimage
+import scipy.ndimage.filters
+import scipy.stats
 import matcompat
 
 # if available import pylab (from matlibplot)
@@ -49,50 +52,114 @@ except ImportError:
 #%                    mtanaka@ctrl.titech.ac.jp                  %
 #%                                                               %
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-def NoiseLevel(img, patchsize, decim, conf, itr):
 
-    # Local Variables: tau, Xtr, DD, num, conf, Dtr, itr, nlevel, cha, img, decim, tau0, th, Dv, eg, imgh, XtrX, sig2, X, patchsize, imgv, d, Xh, Dh, cov, i, kh, p, r, kv, Xv
-    # Function calls: vertcat, sortrows, trace, floor, im2col, double, NoiseLevel, imfilter, rank, gaminv, my_convmtx2, exist, sqrt, eig, Inf, sum, size
-    if not exist('itr', 'var'):
-        itr = 3.
+
+def patchify(img, patch_shape):
+    # http://stackoverflow.com/questions/16774148/fast-way-to-slice-image-into-overlapping-patches-and-merge-patches-to-image
+    img = np.ascontiguousarray(img)  # won't make a copy if not needed
+    X, Y = img.shape
+    x, y = patch_shape
+    shape = ((X-x+1), (Y-y+1), x, y) # number of patches, patch_shape
+    # The right strides can be thought by:
+    # 1) Thinking of `img` as a chunk of memory in C order
+    # 2) Asking how many items through that chunk of memory are needed when indices
+    #    i,j,k,l are incremented by one
+    strides = img.itemsize*np.array([Y, 1, Y, 1])
+    return np.lib.stride_tricks.as_strided(img, shape=shape, strides=strides)
+
+
+def im2col(img, patch_shape):
+    """
+    Matlab's im2col implementation
+    """
+    # Need to transpose image, otherwise result is different from matlab's im2col
+    patch_shape = (patch_shape[1], patch_shape[0])
+    patches = patchify(img.transpose(), patch_shape)
+    num_patches = patches.shape[0] * patches.shape[1]
+    patch_size = patch_shape[0] * patch_shape[1]
+    # 4D to 3D array
+    cols = patches.reshape((num_patches, patch_shape[0], patch_shape[1]))
+    # Final array of cols
+    cols = cols.reshape(num_patches, patch_size)
+    return cols.transpose()
+
+
+def imfilter_h(img, kernel):
+    """
+    Horizontal 1D convolution
+    """
+    if len(img.shape) == 3:
+        # Need to iterate color channels
+        channels = []
+        for channel_idx in xrange(img.shape[2]):
+            channel = np.array([
+                scipy.ndimage.filters.correlate(row, kernel, mode='nearest')
+                for row in img[:,:,channel_idx]
+            ])
+            channels.append(channel)
+        result = np.dstack(tuple(channels))
+    else:
+        # Grayscale image?
+        result = np.array([
+            scipy.ndimage.filters.correlate(row, kernel, mode='nearest')
+            for row in image
+        ])
+    return result
+
+
+def imfilter_v(img, kernel):
+    """
+    Vertical 1D convolution
+    """
+    # Same as horizontal convolution, but done on transposed image
+    return imfilter_h(img.swapaxes(0, 1), kernel).swapaxes(0, 1)
+
+
+def NoiseLevel(img, patchsize=7, decim=0, conf=1-(1E-6), itr=3):
+    # Image has a single channel?
+    if len(img.shape) == 2:
+        new_shape = list(img.shape) + [1]
+        img = img.reshape(tuple(new_shape))
+
+    img = img.astype(np.float64)
+    nlevel, th, num = [], [], []
     
-    
-    if not exist('conf', 'var'):
-        conf = 0.0E-6
-    
-    
-    if not exist('decim', 'var'):
-        decim = 0.
-    
-    
-    if not exist('patchsize', 'var'):
-        patchsize = 7.
-    
-    
-    kh = np.array(np.hstack((-1./2., 0., 1./2.)))
-    imgh = imfilter(img, kh, 'replicate')
+    kernel = [-0.5, 0, 0.5]
+    imgh = imfilter_h(img, kernel)
     imgh = imgh[:,1:matcompat.size(imgh, 2.)-1.,:]
     imgh = imgh*imgh
-    kv = kh.conj().T
-    imgv = imfilter(img, kv, 'replicate')
+
+    # imgv = imfilter(img, kv, 'replicate')
+    # imgv = scipy.ndimage.correlate(img, kv, mode='nearest')
+    imgv = imfilter_v(img, kernel)
     imgv = imgv[1:matcompat.size(imgv, 1.)-1.,:,:]
     imgv = imgv*imgv
-    Dh = my_convmtx2(kh, patchsize, patchsize)
-    Dv = my_convmtx2(kv, patchsize, patchsize)
+
+    kh = np.array([[-1./2.], [0.], [1./2.]])
+    kv = kh.T
+    Dh = my_convmtx2(kh, patchsize)
+    Dv = my_convmtx2(kv, patchsize)
+
     DD = np.dot(Dh.conj().T, Dh)+np.dot(Dv.conj().T, Dv)
-    r = np.rank(DD)
+    r = np.linalg.matrix_rank(DD)
     Dtr = np.trace(DD)
-    tau0 = gaminv(conf, (np.double(r)/2.), matdiv(np.dot(2.0, Dtr), np.double(r)))
-    #%{
-    # eg = linalg.eig(DD)
-    # tau0 = gaminv(conf, (np.double(r)/2.), np.dot(2.0, eg[int(np.dot(patchsize, patchsize))-1]))
-    #%}
+
+    tau0 = scipy.stats.gamma.ppf(
+        conf,
+        (np.double(r)/2.),
+        scale=(2.0 * Dtr / np.double(r)))
+
     for cha in np.arange(1., (matcompat.size(img, 3.))+1):
-        X = im2col(img[:,:,int(cha)-1], np.array(np.hstack((patchsize, patchsize))))
-        Xh = im2col(imgh[:,:,int(cha)-1], np.array(np.hstack((patchsize, patchsize-2.))))
-        Xv = im2col(imgv[:,:,int(cha)-1], np.array(np.hstack((patchsize-2., patchsize))))
-        Xtr = np.sum(vertcat(Xh, Xv))
-        if decim > 0.:
+        X = im2col(img[:, :, int(cha) - 1], (patchsize, patchsize))
+        Xh = im2col(imgh[:, :, int(cha) - 1], (patchsize, patchsize - 2))
+        Xv = im2col(imgv[:, :, int(cha) - 1], (patchsize - 2, patchsize))
+        
+        # Sum columns
+        Xtr = np.sum(np.vstack((Xh, Xv)), 0)
+        # import pdb; pdb.set_trace()
+
+        if decim > 0:
+            # This is not executed by default
             XtrX = vertcat(Xtr, X)
             XtrX = sortrows(XtrX.conj().T).conj().T
             p = np.floor(matdiv(matcompat.size(XtrX, 2.), decim+1.))
@@ -100,40 +167,51 @@ def NoiseLevel(img, patchsize, decim, conf, itr):
             Xtr = XtrX[0,int(p)-1]
             X = XtrX[1:matcompat.size(XtrX, 1.),int(p)-1]
         
-        
+
         #%%%%% noise level estimation %%%%%
-        tau = Inf
+        tau = float('inf')
         if matcompat.size(X, 2.)<matcompat.size(X, 1.):
             sig2 = 0.
         else:
-            cov = matdiv(np.dot(X, X.conj().T), matcompat.size(X, 2.)-1.)
-            d = linalg.eig(cov)
-            sig2 = d[0]
-            
+            cov = np.dot(X, X.T) / (matcompat.size(X, 2.)-1.)
+            d = np.linalg.eigvals(cov)
+            # MATLAB's 'eig' sorts values in asc order!
+            # sig2 = d[0]
+            sig2 = sorted(d)[0]
         
         for i in np.arange(2., (itr)+1):
-            #%%%%% weak texture selectioin %%%%%
-            
-        nlevel[int(cha)-1] = np.sqrt(sig2)
-        th[int(cha)-1] = tau
-        num[int(cha)-1] = matcompat.size(X, 2.)
-        
-    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    return [nlevel, th, num]
-def my_convmtx2(H, m, n):
+            tau=sig2 * tau0
+            p=(Xtr < tau)
+            Xtr = Xtr[p]
+            X = X.T[p].T
+            if (matcompat.size(X,2) < matcompat.size(X,1)):
+                break
+            cov=np.dot(X, X.T) / (matcompat.size(X,2) - 1)
+            d=np.linalg.eigvals(cov)
+            sig2 = sorted(d)[0]
 
-    # Local Variables: i, H, k, j, m, n, p, s, T
-    # Function calls: my_convmtx2, zeros, size
-    s = matcompat.size(H)
-    T = np.zeros(np.dot(m-s[0]+1., n-s[1]+1.), np.dot(m, n))
-    k = 1.
-    for i in np.arange(1., (m-s[0]+1.)+1):
-        for j in np.arange(1., (n-s[1]+1.)+1):
-            for p in np.arange(1., (s[0])+1):
-                T[int(k)-1,int(np.dot(i-1.+p-1., n)+j-1.+1.)-1:np.dot(i-1.+p-1., n)+j-1.+1.+s[1]-1.] = H[int(p)-1,:]
-                
-            k = k+1.
-            
-        
-    return [T]
+        nlevel.append(np.sqrt(sig2))
+        th.append(tau)
+        num.append(matcompat.size(X, 2.))
+
+    return nlevel, th, num
+
+
+def my_convmtx2(kernel, patch_size):
+    """
+    TODO: figure out what the code below actually does, lol
+    """
+    s = kernel.shape
+    m = patch_size - s[0] + 1
+    n = patch_size - s[1] + 1
+    mtx = np.zeros((m * n, patch_size ** 2))
+
+    k = 0
+    for i in xrange(0, m):
+        for j in xrange(0, n):
+            for p in xrange(0, s[0]):
+                l1 = (i + p) * patch_size + j
+                l2 = l1 + s[1]
+                mtx[k, l1:l2] = kernel[p, :]
+            k += 1
+    return mtx
--- projects/denoising/noiselevel/demo.py
+++ projects/denoising/noiselevel/demo.py
@@ -2,6 +2,9 @@
 import numpy as np
 import scipy
 import matcompat
+from skimage import io
+from skimage import data
+from skimage import util
 
 # if available import pylab (from matlibplot)
 try:
@@ -9,20 +12,30 @@ try:
 except ImportError:
     pass
 
-img = np.double(plt.imread('lena.png'))
+from NoiseLevel import NoiseLevel
+
+# img = io.imread('lena.png')
+img = data.lena()
+
+# img = img[99:150,99:150,:]
+# img = img[49:100,49:100,:]
+
 mskflg = 0.
 #% if you want to produce the mask, put one for mskflg
-level = np.array(np.hstack((5., 10., 20., 40.)))
-for i in np.arange(1., (matcompat.size(level, 2.))+1):
-    noise = img+np.dot(plt.randn(matcompat.size(img)), level[int(i)-1])
-    tic
-    [nlevel, th] = NoiseLevel(noise)
-    t = toc
-    fprintf('True: %5.2f  R:%5.2f G:%5.2f B:%5.2f\n', level[int(i)-1], nlevel[0], nlevel[1], nlevel[2])
-    fprintf('Calculation time: %5.2f [sec]\n\n', t)
-    if mskflg:
-        msk = WeakTextureMask(noise, th)
-        imwrite(np.uint8((msk*255.)), sprintf('msk%02d.png', level[int(i)-1]))
+# level = np.array(np.hstack((0, 5, 10, 20, 40)))
+levels = [5, 10, 20, 40]
+# levels = [5]
+for level in levels:
+    noisy_image = util.img_as_ubyte(
+        util.random_noise(img, mode='gaussian', var=pow(float(level) / 256.0, 2))
+    )
+    nlevel, th, num = NoiseLevel(noisy_image)
+    # nlevel, th, num = NoiseLevel(img)
+
+    # fprintf('True: %5.2f  R:%5.2f G:%5.2f B:%5.2f\n', level[int(i)-1], nlevel[0], nlevel[1], nlevel[2])
     
-    
-    
+    # true_sigma = float(level) / 256.0
+    print 'True: %5.2f  R:%5.2f G:%5.2f B:%5.2f' % (level, nlevel[0], nlevel[1], nlevel[2])
+    # if mskflg:
+    #     msk = WeakTextureMask(noise, th)
+    #     imwrite(np.uint8((msk*255.)), sprintf('msk%02d.png', level[int(i)-1]))
